dd6c5e31f06f8352fc0402967b3ab8c47292e932
Viacheslav Tarasov <tvs@niiet.ru>
2021-11-17 11:04:01 +0300

    write vip slave logic

--- tb/tb_vip_slave.sv
+++ tb/tb_vip_slave.sv
@@ -28,6 +28,27 @@ module tb_vip_slave
 
 import cross_bar_pkg::*;
 
+logic [255: 0] mem [DATA_W - 1: 0]; // just for test use only 8 bits
+
+always_ff @(posedge clk)
+    if (slave_req && slave_cmd)
+        mem[slave_addr[7:0]] <= slave_wdata;
+
+assign slave_rdata = (slave_req && !slave_cmd) ? mem[slave_addr[7:0]] : 32'hxxxxxxxx;
+
+always_ff @(posedge clk or negedge aresetn)
+    if (!aresetn)
+        slave_ack <= 1'b0;
+    else if (slave_req) begin
+        slave_ack <= 1'b1;
+    
+        if (slave_cmd) $display("time = %0t \tS[%0d] \tWRITE: Address[0x%8h] Data[0x%8h]",
+                                        $time, slave_addr[ADDR_W - 1: ADDR_W - 2], slave_addr, slave_wdata); // for 4 slaves
+                 
+        else           $display("time = %0t \tS[%0d] \t READ: Address[0x%8h] Data[0x%8h]",
+                                        $time, slave_addr[ADDR_W - 1: ADDR_W - 2], slave_addr, slave_rdata); // for 4 slaves
+    end else
+        slave_ack <= 1'b0;
 
 
 endmodule
